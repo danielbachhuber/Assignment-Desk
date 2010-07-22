59f100e44ba03642a25179fdbb2401e85839e3e0
danielbachhuber <danielbachhuber@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-22 14:57:38 +0000

    Initial commit
    git-svn-id: http://svn.wp-plugins.org/assignment-desk/trunk@265943 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assignment-desk.php
@@ -0,0 +1,8 @@
+/*
+Plugin Name: Assignment Desk
+Version: 0.1
+Plugin URI: http://studio20nyu.tumblr.com/
+Description: Empower the community to guide and participate in the editorial process.
+Author: Erik Froese, Tal Safran, Daniel Bachhuber
+Author URI: http://studio20nyu.tumblr.com/
+*/ 
---
+++ readme.txt
@@ -0,0 +1,8 @@
+=== Assignment Desk ===
+Contributors: efroese, danielbachhuber
+Tags: editorial workflow, journalism, citizen journalism
+Requires at least: 2.9.1
+Tested up to: 3.0
+Stable tag: N/A
+
+Empower the community to guide and participate in the editorial process.
