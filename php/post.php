38c6802b73903ab8bd26b80da8955a367624f450
efroese <efroese@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-30 04:31:45 +0000

    Change the class name to ad_post_meta_box. Changing pitch state works. Show pitched_by. Set pitched_by if empty. Add placeholders for single author or co-authors (Not implemented)
    git-svn-id: http://svn.wp-plugins.org/assignment-desk/trunk@269157 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php/post.php
+++ php/post.php
@@ -1,7 +1,12 @@
 <?php
 if (!class_exists('assignment_desk_post_meta')) {
+	
+require_once(ABSPATH . 'wp-includes/pluggable.php');
 
-class assignment_desk_post_meta {
+/**
+* Post meta_box base class for the assignment desk.
+*/
+class ad_post_meta_box {
     
    function __construct() {
 		// Set up metabox and related actions
@@ -12,7 +17,7 @@ class assignment_desk_post_meta {
 	}
 	
 	/**
-	 * Adds Edit Flow meta_box to Post/Page edit pages 
+	 * Adds Assignment Desk meta_box to Post/Page edit pages 
 	 */
 	function add_post_meta_box() {
 		global $assignment_desk;
@@ -20,7 +25,7 @@ class assignment_desk_post_meta {
 		if (function_exists('add_meta_box')) {
 			add_meta_box('assignment-desk', 
 			                __('Assignment Desk', 'assignment-desk'), 
-			                array(&$this, 'post_meta_box'), 
+			                array(&$this, 'meta_box'), 
 			                'post', 
 			                'side', 
 			                'high');
@@ -29,29 +34,29 @@ class assignment_desk_post_meta {
 		}
 	}
 	
+	/**
+	 * Adds Assignment Desk CSS to Post/Page edit pages 
+	 */
 	function add_css(){
 	    // Enqueue the ad_post_meta.css
 	    wp_enqueue_style('ad-post-meta-style', ASSIGNMENT_DESK_URL.'css/post_meta.css', false, false, 'all');
 	    wp_enqueue_style('ad-fancybox', ASSIGNMENT_DESK_URL . 'js/fancybox/jquery.fancybox-1.3.1.css', false, false, 'all');
 	}
 	
+	/**
+	 * Adds Assignment Desk javascript to Post/Page edit pages 
+	 */
 	function add_javascript(){
 	    wp_enqueue_script('jquery-fancybox-js', ASSIGNMENT_DESK_URL .'js/fancybox/jquery.fancybox-1.3.1.pack.js', 
                     array('jquery'));
         wp_enqueue_script('ad-post-js', ASSIGNMENT_DESK_URL .'js/post.js', array('jquery'));
 	}
-	
-	function post_meta_box(){
-	    global $post, $edit_flow;
-		$user = wp_get_current_user();
-		
-		if(current_user_can('editor')){
-		    $this->read_write_post_meta_box();
-		}
-		else {
-		    $this->read_only_post_meta_box();
-		}
-	}
+}
+
+/**
+* Read-only post meta_box for contributors.
+*/
+class ad_contributor_meta_box extends ad_post_meta_box {
 	
 	function print_pitch_info(){
 	    global $post;
@@ -85,70 +90,7 @@ class assignment_desk_post_meta {
 	    }
 	}
 	
-	function read_write_post_meta_box(){
-	    global $post, $wpdb, $edit_flow, $assignment_desk;
-	    
-	    $this->print_pitch_info();
-	    ?>
-	    
-		<div class="ad-module misc-pub-section">
-		
-		    <h4>Assign this post to:</h4>
-		    <form>
-		        <label for="ad_user_search_text">User</label>
-		        <input type="text" name="ad_user_search" id="ad_user_search_text" size="30" maxlength="50" value="Search...">
-		        <br>
-		        
-		        <label for="ad_assigned_role_select">Role</label>
-		        <select name="ad_assigned_role" id="ad_assigned_role_select">
-		            <option value="writer">Writer</option>
-		            <option value="photographer">Photographer</option>
-		        </select>
-		        <button class="button" value="submit">Assign</button>
-		    </form>
-		</div>
-		
-		<div class="ad-module misc-pub-section">
-			<h4>Users assigned to this post </h4>
-			<ul class="user-list">
-			    <li>Tommy Tester (Writer)</li>
-			    <li>Wendy Writer (Photographer)</li>
-			</ul>
-		</div>
-<?php
-	    
-	    // Print the current Assignees (if any)
-	    
-	    // Print the volunteers (if any and came from a pitch)
-	    $volunteers = array();
-
-?>	    
-	    <div class="ad-module misc-pub-section">
-            <h4>Volunteers (<?php echo count($volunteers); ?>)</h4>
-            <ul>
-            <?php foreach($volunteers as $user): ?>
-                <li>
-                    <?php if (!empty($user->user_login)): ?>
-                        <a href="?page=assignment_desk-contributor&user_login=<?php echo $user->user_login; ?>">
-                            <?php echo $user->user_login; ?></a>
-                            <form method="GET" style="display:inline">
-                                <input type="hidden" name="page" value="assignment_desk-assignments">
-                                <input type="hidden" name="action" value="editor_assign">
-                                <input type="hidden" name="pitch_id" value="<?php echo $pitch->pitch_id; ?>">
-                                <input type="hidden" name="user_login_text" value="<?php echo $user->user_login ?>">
-                                <button>Assign</button>
-                            </form>
-                    <?php else: ?>
-                        <?php echo $user->user_nicename; ?> 
-                    <?php endif; ?>
-                </li>
-            <?php endforeach; ?>
-            </ul>
-        </div>
-<?php
-	}
-	
-	function read_only_post_meta_box($pitch = null){
+	function meta_box($pitch = null){
 	    global $post, $edit_flow, $assignment_desk;
 	    
 	    // Show the due date
@@ -178,7 +120,228 @@ class assignment_desk_post_meta {
 	}
 }
 
-$assignment_desk_post_meta = new assignment_desk_post_meta();
+/**
+* Read-write post meta_box for editors
+*/
+class ad_editor_post_meta_box extends ad_post_meta_box {
+	
+	/**
+	* Print a the meta_box fragment that shows a form to choose the person
+	* who pitched the story. 
+	*
+	* The ID of the person who pitched the story is saved as a
+	* custom field under the key _ad_pitched_by.
+ 	*
+	* If the person who pitched the story is NOT currently a member of the
+	* WP blog we store their email address in the _ad_pitched_by field;
+	* 
+	* When the post is saved we try to look up the user by email. Maybe
+	* they became a member or were assigned a story.
+	*/
+	function pitch_info(){
+		global $post, $wpdb;
+	    echo '<h4><a id="toggle-ad-pitch-detail">Pitch</a></h4>';
+	    echo "<div id='ad-pitch-detail'>";
+	    
+        $pitched_by = get_post_meta($post->ID, '_ad_pitched_by', true);
+		$users = $wpdb->get_results("SELECT ID, user_nicename
+									 FROM $wpdb->users");
+		echo "<label>Pitched by:</label>";
+		
+		// Try to resolve the user email to a user
+		if (is_email($pitched_by)){
+			$user = $wpdb->get_row("SELECT ID, user_nicename 
+									FROM $wpdb->users 
+									WHERE user_email = %s", $pitched_by);
+			if ($user){
+				$pitched_by = $user->ID;
+			}
+			else {
+				echo $pitched_by;
+			}
+		}
+		// Only display a form if 
+		if(!is_email($pitched_by)){
+			echo "<select name='_ad_pitched_by'>";
+			foreach($users as $user){
+				echo "<option value='{$user->ID}'";
+				if ($user->ID == $pitched_by){
+					echo ' selected';
+				}
+				echo ">{$user->user_nicename}</option>";
+			}
+			echo "</select>";
+		}
+
+	    // TODO - Origin? (community or staff)
+	    echo '</div>';
+	}
+	
+	/**
+	* Print the pitch status form.
+	* If there is no status the pitch is New.
+	* If the post is not in the pitch status don't show the form.
+	*/
+	function pitch_status(){
+		global $post, $wpdb, $assignment_desk;
+
+		echo '<label>Pitch Status:</label>';
+		// What is the status of this Pitch?
+		$current_status = wp_get_object_terms($post->ID,
+											  $assignment_desk->custom_pitch_statuses->taxonomy);
+		// Default status is new
+		// TODO - Use a default setting.
+		if(!$current_status){
+			$current_status = get_term_by("name", "New", $assignment_desk->custom_pitch_statuses->taxonomy);
+		}
+		else {
+			$current_status = $current_status[0];
+		}
+		
+		if($post->post_status == 'pitch'){
+			// List all of the pitch statuses
+			$pitch_statuses = get_terms($assignment_desk->custom_pitch_statuses->taxonomy,
+										array( 'get' => 'all'));
+			echo "<select name='_ad_pitch_status'>";
+			foreach($pitch_statuses as $status){
+				echo "<option value='{$status->term_id}' ";
+				if($status->term_id == $current_status->term_id){
+					echo "selected";
+				}
+				echo ">{$status->name}</option>";
+			}
+			echo "</select>";
+		}
+	}
+	
+	/**
+	* Print a form to choose the user.
+	*/
+	function author(){
+		
+	}
+	
+	/**
+	* Print a form to choose multiple users.
+	* Print the current lists of assignees.
+	*/
+	function multiple_assignees() {
+		?>
+		<div class="ad-module misc-pub-section">
+		    <h4>Assign this post to:</h4>
+		    <form>
+		        <label for="ad_user_search_text">User</label>
+		        <input type="text" name="_ad_user_search" id="ad_user_search_text" size="30" maxlength="50" value="Search...">
+		        <br>
+		        
+		        <label for="ad_assigned_role_select">Role</label>
+		        <select name="_ad_assigned_role" id="ad_assigned_role_select">
+		            <option value="writer">Writer</option>
+		            <option value="photographer">Photographer</option>
+		        </select>
+		        <button class="button" value="submit">Assign</button>
+		    </form>
+		</div>
+		
+		<div class="ad-module misc-pub-section">
+			<h4>Users assigned to this post </h4>
+			<ul class="user-list">
+			    <li>Tommy Tester (Writer)</li>
+			    <li>Wendy Writer (Photographer)</li>
+			</ul>
+		</div>
+<?php
+	}
+	
+	/**
+	* Print the list of volunteers.
+	*/
+	function volunteers(){
+		global $post;
+		// Print the volunteers (if any and came from a pitch)
+	    $volunteers = get_post_meta($post->ID, '_ad_volunteer', false);
+
+		if(count($volunteers)): ?>	    
+	    <div class="ad-module misc-pub-section">
+            <h4>Volunteers (<?php echo count($volunteers); ?>)</h4>
+            <ul>
+            <?php foreach($volunteers as $user_id):
+					$user = get_userdata((int)$user_id);
+				?>
+                <li>
+                <?php if (!empty($user->user_login)): ?>
+                    <a href="?page=assignment_desk-contributor&user_login=<?php echo $user->user_login; ?>">
+                    <?php echo $user->user_login; ?></a>
+                    <form method="GET" style="display:inline">
+                        <input type="hidden" name="page" value="assignment_desk-assignments">
+                        <input type="hidden" name="action" value="editor_assign">
+                        <input type="hidden" name="pitch_id" value="<?php echo $pitch->pitch_id; ?>">
+                        <input type="hidden" name="user_login_text" value="<?php echo $user->user_login ?>">
+                        <button>Assign</button>
+                    </form>
+                <?php else: ?>
+                    <?php echo $user->user_nicename; ?> 
+                <?php endif; ?>
+                </li>
+            <?php endforeach; ?>
+            </ul>
+        </div>
+		<?php else: ?>
+			<i>No Volunteers</i>
+		<?php endif; ?>
+<?php
+	}
+	
+	/**
+	* Launch the Assignment Desk post meta_box.
+	*/
+	function meta_box(){
+	    global $post, $wpdb, $edit_flow, $assignment_desk;
+	    
+		echo '<div id="ad-pitch-info" class="ad-module misc-pub-section">';
+	    $this->pitch_info();
+		if($post->post_status == __('pitch')){
+			$this->pitch_status();
+		}
+		echo '</div>';
+	
+		if (is_plugin_active('co-authors/co-authors.php')){
+		 	$this->multiple_assignees();
+		}
+		else {
+			$this->author();
+		}
+
+		$this->volunteers();
+	}
+	
+	/**
+	* Save Assignment Desk post meta data
+	*/
+	function save_post_meta_box($post_id) {
+		global $wpdb, $assignment_desk;
+
+		if($_POST['_ad_pitched_by']){
+			update_post_meta($post_id, '_ad_pitched_by', (int)$_POST['_ad_pitched_by']);
+		}
+		
+		if($_POST['_ad_pitch_status']){
+			wp_set_object_terms($post_id, 
+								(int)$_POST['_ad_pitch_status'],
+								$assignment_desk->custom_pitch_statuses->taxonomy);
+		}
+	}
+}
+
+$user = wp_get_current_user();
+$assignment_desk_post_meta = null;
+
+if(current_user_can(5)){
+	$assignment_desk_post_meta = new ad_editor_post_meta_box();
+}
+else {
+	$assignment_desk_post_meta = new ad_contributor_meta_box();
+}
 
 } // end if(!class_exists)
 ?>
