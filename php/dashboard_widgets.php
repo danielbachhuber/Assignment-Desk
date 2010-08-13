6f0cb276986fea9ee52ff7156508c7602bb645f8
efroese <efroese@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-13 18:41:11 +0000

    Add a dashboard widget for Assignments. Show Pending assignments with links. Links do not work yet.
    git-svn-id: http://svn.wp-plugins.org/assignment-desk/trunk@275539 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php/dashboard_widgets.php
+++ php/dashboard_widgets.php
@@ -21,9 +21,9 @@ class ad_dashboard_widgets {
            // if ($assignment_desk->get_plugin_option('editor_overview_widget_enabled')) {
                wp_add_dashboard_widget('ad_editor_overview', 'Story Pitches', array(&$this, 'editor_overview_widget'));
            // }
-           
-           wp_add_dashboard_widget('ad_assignments', 'Assignments', array(&$this, 'assignments_widget'));
-        }    
+        }
+        
+        wp_add_dashboard_widget('ad_assignments', 'Assignments', array(&$this, 'assignments_widget'));
     }
 
 	function count_pitches($status){
@@ -68,29 +68,46 @@ class ad_dashboard_widgets {
 <?php
    }
    
-   function assignments_widget(){
+    function assignments_widget(){
+        global $assignment_desk, $wpdb, $current_user;
        
-       // Count the number of pending posts this user has.
-       // Query the post meta table by key _ad_partitipant$user_login
-       // Iterate over those custom fields and pull (post ID => ($role_id, ...) where user has a 'pending' role record. 
-       
-       // In Progress
-       // @todo - Do we need this? Co-authors plus handles the "Right Now" widget display
+        get_currentuserinfo();
+        $pending_posts = array();
+        
+        // Find all of the posts this user is a participant in.
+        $participant_posts = $wpdb->get_results("SELECT post_id FROM $wpdb->postmeta 
+                                                  WHERE meta_key = '_ad_participant_{$current_user->user_login}'
+                                                  ORDER BY post_id");
+        if ( ! $participant_posts ){
+            $participant_posts = array();
+        }
+        
+        $roles = $assignment_desk->custom_taxonomies->get_user_roles();
+        
+        foreach($participant_posts as $post ){
+            foreach($roles as $user_role){
+                // Get all fo the roles this user has for this post
+                $participant_record = get_post_meta($post->post_id, "_ad_participant_role_$user_role->term_id", true);
+                if(!$participant_record) { $participant_record = array(); }
+                foreach ( $participant_record as $user_login => $status ){
+                    if( $user_login == $current_user->user_login && $status == 'pending' ){
+                        $pending_posts[] = array($post->post_id, $user_role);
+                    }
+                }
+            }
+        }
+        $count_pending = count($pending_posts);
 ?>
-<div class="table">
-<table>
-    <tbody>
-        <tr>
-            <td class="t">0</td>
-            <td class="label">Pending</td>
-        </tr>
-        <tr>
-            <td class="t">0</td>
-            <td class="label">In Progress</td>
-        </tr>
-    </tbody>
-</table>
-</div>
+<p class="sub"><?php echo $count_pending; ?> pending assignment<?php echo ($count_pending != 1)? 's': ''?>.</p>
+<?php foreach($pending_posts as $pending): ?>
+    <div>
+        <?php $post = get_post($pending[0]); ?>
+        <?php echo $post->post_title ?> | <?php echo $pending[1]->name; ?>
+        <a href="?invite_response=accepted">Accept</a>
+        <a href="?invite_response=declined">Decline</a>
+    </div>
+<?php endforeach; ?>
+
 <div class="inside">&raquo; <a href="?page=assignment_desk-index">Go to Assignment Desk landing page.</a></div>
 <?php
    }
