748a48c403f1565d390d57193a6445e855752dc5
efroese <efroese@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-31 04:13:20 +0000

    Renamed posts query function and fixed MySQL syntax to not use joins
    git-svn-id: http://svn.wp-plugins.org/assignment-desk/trunk@283022 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php/utils.php
+++ php/utils.php
@@ -105,26 +105,28 @@ function get_inprogress_posts(){
  * Get a list of posts that should be displayed to the public for feedback.
  * Returns unpublished posts that do not have an assignment_status of completed and are not private.
  */
-if (!function_exists('get_public_feedback_posts')){
-function get_public_feedback_posts(){
+if ( !function_exists('ad_get_all_public_posts') ) {
+function ad_get_all_public_posts( $args = null ) {
     global $assignment_desk, $wpdb;
     $include_statuses = implode(',', $assignment_desk->public_facing_options['public_facing_assignment_statuses']);
     if ( ! $include_statuses ) {
         $include_statuses = -1;
     }
-    $results = $wpdb->get_results("SELECT * FROM $wpdb->posts
-                                LEFT JOIN $wpdb->postmeta ON($wpdb->posts.ID = $wpdb->postmeta.post_id)
-                                LEFT JOIN $wpdb->term_relationships ON($wpdb->posts.ID = $wpdb->term_relationships.object_id)
-                                LEFT JOIN $wpdb->term_taxonomy ON($wpdb->term_relationships.term_taxonomy_id = $wpdb->term_taxonomy.term_taxonomy_id)
-                                LEFT JOIN $wpdb->terms ON($wpdb->term_taxonomy.term_id = $wpdb->terms.term_id)
-                                WHERE $wpdb->posts.post_type = 'post' 
-                                AND $wpdb->posts.post_status != 'publish'
-                                AND $wpdb->posts.post_status != 'trash'
-                                AND $wpdb->term_taxonomy.taxonomy = '{$assignment_desk->custom_taxonomies->assignment_status_label}'
-                                AND $wpdb->terms.term_id IN ({$include_statuses})
-                                ORDER BY $wpdb->posts.post_date DESC");
-    if(!$results){
-        $results = array();
+
+	$query = "SELECT $wpdb->posts.* FROM $wpdb->posts, $wpdb->term_relationships
+						WHERE $wpdb->posts.post_type = 'post' 
+						AND $wpdb->posts.post_status != 'publish'
+						AND $wpdb->posts.post_status != 'trash'
+						AND $wpdb->posts.post_status != 'auto-draft'
+						AND $wpdb->posts.post_status != 'inherit'
+						AND ( $wpdb->posts.ID = $wpdb->term_relationships.object_id
+							AND $wpdb->term_relationships.term_taxonomy_id IN ({$include_statuses}) )
+						ORDER BY $wpdb->posts.post_date DESC";
+
+	$results = $wpdb->get_results( $query );
+	
+    if ( !$results ) {
+        $results = false;
     }
     return $results;
 }
