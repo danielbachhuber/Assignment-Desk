3c5061023493b981daeb19b185a4d7f619be021a
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-10-11 21:46:59 -0400

    Move all of the prior all_votes data to our custom table upon upgrade to v0.8

--- php/upgrade.php
+++ php/upgrade.php
@@ -18,9 +18,22 @@ class ad_upgrade {
 	}
 	
 	function upgrade_08() {
-		global $assignment_desk;
+		global $assignment_desk, $wpdb;
 		
-		//update_option( $assignment_desk->get_plugin_option_fullname('version'), '0.8' );
+		$query = "SELECT * FROM $wpdb->postmeta WHERE meta_key='_ad_votes_all';";
+		$results = $wpdb->get_results( $query );
+		if ( $results ) {
+			foreach ( $results as $result ) {
+				$meta_value = unserialize( $result->meta_value );
+				foreach ( $meta_value as $key => $user_id ) {
+					$assignment_desk->public_views->update_user_vote_for_post( $result->post_id, $user_id );
+				}
+				$query = "DELETE FROM $wpdb->postmeta WHERE meta_key='_ad_votes_all' AND post_id=$result->post_id;";
+				$wpdb->get_results( $query );
+			}
+		}
+		
+		update_option( $assignment_desk->get_plugin_option_fullname('version'), '0.8' );
 	}
 	
 	
