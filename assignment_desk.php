aa21961ebcc342e235c442222fd12af775d37691
Daniel Bachhuber <d@danielbachhuber.com>
2011-03-28 17:34:04 -0400

    Text updates
    [#11631163]

--- assignment_desk.php
+++ assignment_desk.php
@@ -3,12 +3,12 @@
 Plugin Name: Assignment Desk
 Plugin URI: http://openassignment.org/
 Description: News pitch and story tools for local news blogs.
-Author: Erik Froese, Daniel Bachhuber
+Author: Daniel Bachhuber, Erik Froese
 Version: 0.9.4
 Author URI: http://openassignment.org/
 */   
    
-/*  Copyright 2010  
+/*  Copyright 2010-2011
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
